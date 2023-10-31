4eb9de3a12227c17b799775043706f109e49cf13
ricardojonathanromero <ricardo.jonathan.romero@gmail.com>
2023-10-30 21:41:43 -0600

    add radix sort bench and fuzz tests

--- sort/radix/solution_fuzz_test.go
+++ sort/radix/solution_fuzz_test.go
@@ -1,1 +1,37 @@
 package radix
+
+import (
+	"github.com/ricardojonathanromero/basics/utils/sort"
+	"testing"
+)
+
+func fuzzRadixSort(t *testing.T, title string, input []int) {
+	t.Logf("start %s test", title)
+	// Sort the array using Bubble Sort
+	radixSort(input)
+
+	// Verify that the array is sorted
+	if !sort.IntSorted(input) {
+		t.Errorf("Radix Sort failed to sort the array: %v", input)
+	}
+}
+
+func fuzzRadixSortOptimal(t *testing.T, title string, input []int) {
+	t.Logf("start %s test", title)
+	// Sort the array using Bubble Sort
+	radixSortOptimized(input)
+
+	// Verify that the array is sorted
+	if !sort.IntSorted(input) {
+		t.Errorf("Radix Sort failed to sort the array: %v", input)
+	}
+}
+
+func FuzzAll(f *testing.F) {
+	f.Add("radix_sort_fuzz_test")
+	f.Fuzz(func(t *testing.T, s string) {
+		input := sort.GenerateRandomIntSlice(1000)
+		fuzzRadixSortOptimal(t, s, input)
+		fuzzRadixSort(t, s, input)
+	})
+}
